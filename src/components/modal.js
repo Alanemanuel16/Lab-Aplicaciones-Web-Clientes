c6ee25fc7c4e43b93f318d3c1588216250ee4485
Alanemanuel16 <alanarraygada45@gmail.com>
2026-09-12 21:32:39 -0300

    cambio al hacer click en mas detalles aparece titulo-imagen-descripcion del articulo

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,28 +1,26 @@
-export function Modal(){
+export function Modal(p){
 
     let container = document.querySelector('#productModal');
 
-
     let template = `
-     <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">${getProducts.title}</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <img src="${getProducts.image}" class="img-fluid" alt="${getProducts.title}">
-      <p>${getProducts.description}</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+     <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="productModalLabel">${p.title}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <img src="${p.image}" class="img-fluid mb-3" alt="${p.title}" style="max-height: 300px; object-fit: contain;">
+                <p class="card-text">${p.description}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
     </div>
- 
   `;
 
-  container.innerHTML= template;
+  container.innerHTML = template;
 
   const bootstrapModal = new bootstrap.Modal(container);
   bootstrapModal.show();

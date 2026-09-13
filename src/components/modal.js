9eb01c0c4347f5dec384b955ee879264d6a8bb3c
Alanemanuel16 <alanarraygada45@gmail.com>
2026-09-12 21:13:12 -0300

    agregado linea 14 a la 15 en modal.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -11,7 +11,8 @@ export function Modal(){
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+      <img src="${getProducts.image}" class="img-fluid" alt="${getProducts.title}">
+      <p>${getProducts.description}</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>

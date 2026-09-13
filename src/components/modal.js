0dd74fe04555b62befd5a228c2f4a576fab5d9cf
Alanemanuel16 <alanarraygada45@gmail.com>
2026-09-12 22:10:32 -0300

    cambien el nombre del boton secundario y el idioma de close a cerrar

--- src/components/modal.js
+++ src/components/modal.js
@@ -20,8 +20,8 @@ export function Modal(p){
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">agregar al carrito</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Agregar al carrito</button>
             </div>
         </div>
     </div>

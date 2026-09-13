8b3044f858f97a3701ef714f5c1cc8a263a33c8b
Alanemanuel16 <alanarraygada45@gmail.com>
2026-09-12 22:01:54 -0300

    otra modificacion en la card grid pero en descripcion y titulo y en imagen de 200px a 180px y de 1rem a 0.75rem

--- src/components/cards.js
+++ src/components/cards.js
@@ -10,7 +10,7 @@ export function RenderCards(){
         template += `
         <div class="col">
             <div class="card justify-content-center aling item center h-100">
-                <img src="${p.image}" class="card-img-top img-fluid" alt="${p.title}" style="height: 200px; width: 100%; object-fit: contain; padding: 1rem;">
+                <img src="${p.image}" class="card-img-top img-fluid" alt="${p.title}" style="height: 180px; width: 100%; object-fit: contain; padding: 0.75rem;">
                 <div class="card-body" style="h-100">
                     <h5 class="card-title text-truncate">${p.title}</h5>
                 </div>
--- src/components/modal.js
+++ src/components/modal.js
@@ -9,15 +9,16 @@ export function Modal(p){
                 <h1 class="modal-title fs-5" id="productModalLabel">${p.title}</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body text-center">
-            <div class="row">
-            <div class="col-md-6">
-             <img src="${p.image}" class="img-fluid mb-3" alt="${p.title}" style="max-height: 300px; object-fit: contain;">
-            </div>
-            <div class="col-md-6>
-            <p class="card-text">${p.description}</p>
-            </div>
-            </div>
+            <div class="modal-body">
+                <div class="row g-3 align-items-center">
+                    <div class="col-md-4">
+                        <img src="${p.image}" class="img-fluid rounded" alt="${p.title}">
+                    </div>
+                    <div class="col-md-8">
+                        <h5 class="card-title">${p.title}</h5>
+                        <p class="card-text mb-0">${p.description}</p>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>

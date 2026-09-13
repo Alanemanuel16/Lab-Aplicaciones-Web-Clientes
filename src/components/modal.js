49b88765f7f8ff2dcb9bc69f7577deed5da0d8fa
Alanemanuel16 <alanarraygada45@gmail.com>
2026-09-12 21:54:53 -0300

    modificacion en las card al hacer click en mas detalles

--- src/components/modal.js
+++ src/components/modal.js
@@ -10,8 +10,14 @@ export function Modal(p){
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body text-center">
-                <img src="${p.image}" class="img-fluid mb-3" alt="${p.title}" style="max-height: 300px; object-fit: contain;">
-                <p class="card-text">${p.description}</p>
+            <div class="row">
+            <div class="col-md-6">
+             <img src="${p.image}" class="img-fluid mb-3" alt="${p.title}" style="max-height: 300px; object-fit: contain;">
+            </div>
+            <div class="col-md-6>
+            <p class="card-text">${p.description}</p>
+            </div>
+            </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>

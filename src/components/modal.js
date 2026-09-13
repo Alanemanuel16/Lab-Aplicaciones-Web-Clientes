402756352b29a88e67f779037f5eb05e7181a2b9
Alanemanuel16 <alanarraygada45@gmail.com>
2026-09-12 22:09:06 -0300

    agregue el boton primario agregar-al-carrito

--- src/components/modal.js
+++ src/components/modal.js
@@ -21,6 +21,7 @@ export function Modal(p){
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">agregar al carrito</button>
             </div>
         </div>
     </div>

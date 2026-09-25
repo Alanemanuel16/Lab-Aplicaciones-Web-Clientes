ec180d18e13be815bbf9120ed8ae3e45b271a8f0
Alanemanuel16 <alanarraygada45@gmail.com>
2026-09-25 01:32:03 -0300

    agregado de guardar o registrar el producto en el Local-Storage

--- src/components/storage/storage.js
+++ src/components/storage/storage.js
@@ -35,11 +35,20 @@ export function saveToLocalStorage(item){
     let cart = getFromLocalStorage();
     const index = cart.findIndex(el => el.id === item.id);
     if (index >= 0) {
-        cart[index] = item;
+        cart[index].qtty += item.qtty;
     } else {
         cart.push(item);
     }
     localStorage.setItem(STORAGE_KEY, JSON.stringify(cart));
 }
 
+export function updateItemStorage(itemID, qtty) {
+    let dataStorage = getFromLocalStorage();
+    let idx = dataStorage.findIndex((p) => p.id === itemID);
+    if (idx !== -1){
+        dataStorage[idx].qtty += qtty;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(dataStorage));
+    }
+    return idx;
+}
 
